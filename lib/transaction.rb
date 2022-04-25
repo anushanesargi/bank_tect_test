c45cbb210cff3a7103577d19cb2fa28a4e418ed1
Anusha Nesargi <anushanesargi@gmail.com>
2022-04-25 18:27:14 +0100

    changed min bal, added debit method

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,12 +1,16 @@
 class Transaction
-  MINIMUM_BALANCE = 2000
+  MINIMUM_BALANCE = 3000
 
   def initialize
     @balance = MINIMUM_BALANCE
   end
 
-  def credit(amount_to_be_added)
-    @balance += amount_to_be_added
+  def credit(amount_to_be_credited)
+    @balance += amount_to_be_credited
+  end
+
+  def debit(amount_to_be_debited)
+    @balance -= amount_to_be_debited
   end
 
   def balance
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,14 +3,22 @@ require 'transaction'
 describe ".balance" do
   it "should return the balance in the account" do
     transaction = Transaction.new
-    expect(transaction.balance).to eq(2000)
+    expect(transaction.balance).to eq(3000)
   end
 end
 
 describe ".credit" do
-  it "should add the credit amount the the existing balance" do
+  it "should add the credit amount to the existing balance" do
     transaction = Transaction.new
     transaction.credit(500)
-     expect(transaction.balance).to eq(2500)
+    expect(transaction.balance).to eq(3500)
+  end
+end
+
+describe ".debit" do
+  it "should subtract the debit amount from the existing balance" do
+    transaction = Transaction.new
+    transaction.debit(500)
+    expect(transaction.balance).to eq(2500)
   end
 end

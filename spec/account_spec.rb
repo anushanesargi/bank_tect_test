cfcaccbb55cf8148e71d4d5796624822f27ebae5
Anusha Nesargi <anushanesargi@gmail.com>
2022-04-26 20:24:41 +0100

    withdrawal method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,9 +1,21 @@
 require 'account'
 
-describe ".credit" do
-  it "should add the credit amount to the existing balance" do
-    first_account = Account.new(3000, [])
-    first_account.credit('13/01/2022', 500)
-    expect(first_account.balance).to eq(3500)
+describe Account do
+  first_account = Account.new(3000, [])
+  second_account = Account.new(2000, [])
+  describe ".credit" do
+    it "should add the credit amount to the existing balance" do
+      first_account.credit('13/01/2022', 500)
+      expect(first_account.balance).to eq(3500)
+    end
   end
+  
+  describe ".credit" do
+    it "should add the credit amount to the existing balance" do
+      second_account.withdrawal('13/01/2022', 500)
+      expect(second_account.balance).to eq(1500)
+    end
+  end
+
 end
+

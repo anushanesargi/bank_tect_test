2b1d02ad6fbeb832eccfd6db6bd1fce20bb2f327
Anusha Nesargi <anushanesargi@gmail.com>
2022-04-26 21:06:50 +0100

    statement class to display the transaction statements

---
+++ lib/statement.rb
@@ -0,0 +1,16 @@
+require 'account'
+
+class Statement
+
+  def initialize(instance)
+    @account_instance = instance
+  end
+
+  def statement_print
+    puts "date || credit || debit || balance"
+    @account_instance.passbook.reverse.each do |item|
+      puts "#{item[:date]} || #{item[:credit]} || #{item[:debit]} || #{item[:bal]}"
+    end
+  end
+
+end

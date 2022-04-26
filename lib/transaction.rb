c3271b8c7c2f839ccc0a6136adf42dc4f458ed8d
Anusha Nesargi <anushanesargi@gmail.com>
2022-04-26 19:06:21 +0100

    class methods and objects, date inclusion

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,20 +1,17 @@
 class Transaction
   MINIMUM_BALANCE = 3000
+  @@balance = MINIMUM_BALANCE
 
-  def initialize
-    @balance = MINIMUM_BALANCE
+  def self.credit(date, amount_to_be_credited)
+    @@balance += amount_to_be_credited
   end
 
-  def credit(amount_to_be_credited)
-    @balance += amount_to_be_credited
+  def self.withdrawal(date, amount_to_be_debited)
+    @@balance -= amount_to_be_debited
   end
 
-  def debit(amount_to_be_debited)
-    @balance -= amount_to_be_debited
-  end
-
-  def balance
-    return @balance
+  def self.balance
+    return @@balance
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,23 +2,20 @@ require 'transaction'
 
 describe ".balance" do
   it "should return the balance in the account" do
-    transaction = Transaction.new
-    expect(transaction.balance).to eq(3000)
+    expect(Transaction.balance).to eq(3000)
   end
 end
 
 describe ".credit" do
   it "should add the credit amount to the existing balance" do
-    transaction = Transaction.new
-    transaction.credit(500)
-    expect(transaction.balance).to eq(3500)
+    Transaction.credit('13/01/2022', 500)
+    expect(Transaction.balance).to eq(3500)
   end
 end
 
 describe ".debit" do
   it "should subtract the debit amount from the existing balance" do
-    transaction = Transaction.new
-    transaction.debit(500)
-    expect(transaction.balance).to eq(2500)
+    Transaction.withdrawal('15/01/2022', 500)
+    expect(Transaction.balance).to eq(3000)
   end
 end

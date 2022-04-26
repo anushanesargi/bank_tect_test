284073e1c266a5a99ff4ec3776cec9298f7b533e
Anusha Nesargi <anushanesargi@gmail.com>
2022-04-26 20:30:46 +0100

    logging transactions in the passbook

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,19 +1,28 @@
 require 'account'
 
 describe Account do
-  first_account = Account.new(3000, [])
-  second_account = Account.new(2000, [])
   describe ".credit" do
     it "should add the credit amount to the existing balance" do
+      first_account = Account.new(3000, [])
       first_account.credit('13/01/2022', 500)
       expect(first_account.balance).to eq(3500)
     end
   end
   
-  describe ".credit" do
-    it "should add the credit amount to the existing balance" do
+  describe ".withdrawal" do
+    it "should debit the amount to the existing balance" do
+      second_account = Account.new(2000, [])
       second_account.withdrawal('13/01/2022', 500)
       expect(second_account.balance).to eq(1500)
+    end
+  end
+
+  describe ".passbook" do
+    it "should add the transactions to the passbook" do
+      first_account = Account.new(3000, [])
+      first_account.credit('13/01/2022', 500)
+      first_account.withdrawal('15/01/2022', 500)
+      expect(first_account.passbook).to eq([{date: '13/01/2022', credit: 500, debit: 0, bal: 3500}, {date: '15/01/2022', credit: 0, debit: 500, bal: 3000}])
     end
   end
 
